5e5151fe390d7db66607096f1ad732c9168cfdde
plattdev <natalia.barrientos.m@gmail.com>
2025-10-25 11:27:30 +0200

    orange stars

--- js/stars.js
+++ js/stars.js
@@ -1,9 +1,9 @@
 // Variables for star configuration
 const numStars = 100
+const redStars = 5
 
 
-// Everytime there is a call to stars.html, it will create a random star background
-
+// Everytime there is a call to stars.html, it will create a random white star background
 for (let i = 0; i < numStars; i++) { 
     const star = document.createElement('div') // Create a phantom div element (since it is not explicitly added in the HTML) for each star
     star.classList.add('star') // Add the star class to each star div. This class will be styled in the CSS file where it will be defined a position becase we will use top and left properties to place the stars randomly
@@ -17,6 +17,23 @@ for (let i = 0; i < numStars; i++) {
     star.style.width = Math.random() * 2 + 1 + "px"
     star.style.height = star.style.width //height equal to width
 }
+
+// Everytime there is a call to stars.html, it will create a random red star background
+for (let i = 0; i < redStars; i++) { 
+    const star = document.createElement('div') 
+    star.classList.add('redStar') 
+    document.body.appendChild(star)
+
+    star.style.top = Math.random() * 100 + "dvh" 
+    star.style.left = Math.random() * 100 + "dvw"
+
+    //random size between 1 and 7 pixels
+    star.style.width = Math.random() * 6 + 1 + "px"
+    star.style.height = star.style.width 
+}
+
+
+
 
 
 
